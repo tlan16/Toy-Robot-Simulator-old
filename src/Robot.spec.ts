801087ac3f70b8aae0133bc404f30486f10a9506
Frank Lan <frank.lan@aarnet.edu.au>
2021-12-02 19:35:33 +1100

    add robot facing

--- src/Robot.spec.ts
+++ src/Robot.spec.ts
@@ -1,4 +1,4 @@
-import { Position, Robot } from './Robot'
+import { Facing, Position, Robot } from './Robot'
 import * as assert from 'assert'
 import { Tabletop } from './Tabletop'
 
@@ -8,6 +8,7 @@ describe('Robot', () => {
             const robot = new Robot()
             assert.strictEqual(robot.tabletop, undefined)
             assert.strictEqual(robot.position, undefined)
+            assert.strictEqual(robot.facing, undefined)
         })
     })
 
@@ -16,68 +17,83 @@ describe('Robot', () => {
             const robot = seedRobotOnTabletop({ tabletop: new Tabletop() })
             assert.strictEqual(robot.tabletop instanceof Tabletop, true)
             assert.deepEqual(robot.position, { x: 0, y: 0 })
+            assert.deepEqual(robot.facing, 'NORTH')
         })
-        it('should be placed on a given tabletop with initial position', () => {
+        it('should be placed on a given tabletop with initial values', () => {
             const position: Readonly<Position> = { x: 2, y: 3 }
-            const robot = seedRobotOnTabletop({ position })
+            const facing: Readonly<Facing> = 'EAST'
+            const robot = seedRobotOnTabletop({ position, facing })
             assert.strictEqual(robot.tabletop instanceof Tabletop, true)
             assert.strictEqual(robot.position, position)
+            assert.strictEqual(robot.facing, facing)
         })
     })
 
     describe('move', () => {
         describe('up', () => {
             it('should move up', () => {
-                const robot = seedRobotOnTabletop({ position: { x: 0, y: 0 } })
+                const robot = seedRobotOnTabletop({ position: { x: 0, y: 0 }, facing: 'EAST' })
                 robot.move('UP')
-                assert.deepEqual(robot.position, { x: 0, y: 1 })
+                assert.deepStrictEqual(robot.position, { x: 0, y: 1 })
+                assert.strictEqual(robot.facing, 'NORTH')
             })
             it('should not move up', () => {
                 const robot = seedRobotOnTabletop({ tabletop: new Tabletop(3, 3), position: { x: 1, y: 3 } })
                 robot.move('UP')
-                assert.deepEqual(robot.position, { x: 1, y: 3 })
+                assert.deepStrictEqual(robot.position, { x: 1, y: 3 })
+                assert.strictEqual(robot.facing, 'NORTH')
             })
         })
         describe('down', () => {
             it('should move down', () => {
                 const robot = seedRobotOnTabletop({ position: { x: 3, y: 3 } })
                 robot.move('DOWN')
-                assert.deepEqual(robot.position, { x: 3, y: 2 })
+                assert.deepStrictEqual(robot.position, { x: 3, y: 2 })
+                assert.strictEqual(robot.facing, 'SOUTH')
             })
             it('should not move down', () => {
                 const robot = seedRobotOnTabletop({ tabletop: new Tabletop(3, 3), position: { x: 1, y: 0 } })
                 robot.move('DOWN')
-                assert.deepEqual(robot.position, { x: 1, y: 0 })
+                assert.deepStrictEqual(robot.position, { x: 1, y: 0 })
+                assert.strictEqual(robot.facing, 'SOUTH')
             })
         })
         describe('left', () => {
             it('should move left', () => {
                 const robot = seedRobotOnTabletop({ position: { x: 3, y: 3 } })
                 robot.move('LEFT')
-                assert.deepEqual(robot.position, { x: 2, y: 3 })
+                assert.deepStrictEqual(robot.position, { x: 2, y: 3 })
+                assert.strictEqual(robot.facing, 'WEST')
             })
             it('should not move left', () => {
                 const robot = seedRobotOnTabletop({ tabletop: new Tabletop(3, 3), position: { x: 0, y: 0 } })
                 robot.move('LEFT')
-                assert.deepEqual(robot.position, { x: 0, y: 0 })
+                assert.deepStrictEqual(robot.position, { x: 0, y: 0 })
+                assert.strictEqual(robot.facing, 'WEST')
             })
         })
         describe('right', () => {
             it('should move right', () => {
                 const robot = seedRobotOnTabletop({ position: { x: 3, y: 3 } })
                 robot.move('RIGHT')
-                assert.deepEqual(robot.position, { x: 4, y: 3 })
+                assert.deepStrictEqual(robot.position, { x: 4, y: 3 })
+                assert.strictEqual(robot.facing, 'EAST')
             })
             it('should not move right', () => {
                 const robot = seedRobotOnTabletop({ tabletop: new Tabletop(3, 3), position: { x: 3, y: 3 } })
                 robot.move('RIGHT')
-                assert.deepEqual(robot.position, { x: 3, y: 3 })
+                assert.deepStrictEqual(robot.position, { x: 3, y: 3 })
+                assert.strictEqual(robot.facing, 'EAST')
             })
         })
     })
 })
 
-function seedRobotOnTabletop(options?: { tabletop?: Readonly<Tabletop>; position?: Readonly<Position> }) {
+function seedRobotOnTabletop(options?: {
+    tabletop?: Readonly<Tabletop>
+    position?: Readonly<Position>
+    facing?: Readonly<Facing>
+}) {
     const robot = new Robot()
     const tabletop =
         options?.tabletop ??
@@ -85,6 +101,9 @@ function seedRobotOnTabletop(options?: { tabletop?: Readonly<Tabletop>; position
             options?.position?.x ? options.position.x * 2 : undefined,
             options?.position?.y ? options.position.y * 2 : undefined,
         )
-    robot.placeOnTabletaop(tabletop, options?.position)
+    robot.placeOnTabletaop(tabletop, {
+        initialPosition: options?.position,
+        initialFacing: options?.facing,
+    })
     return robot
 }
--- src/Robot.ts
+++ src/Robot.ts
@@ -6,18 +6,31 @@ export interface Position {
 }
 
 export type Direction = 'UP' | 'DOWN' | 'LEFT' | 'RIGHT'
+export type Facing = 'NORTH' | 'SOUTH' | 'WEST' | 'EAST'
 
 export class Robot {
     #position: Position = { x: 0, y: 0 }
+    #facing: Facing = 'NORTH'
     public tabletop: Tabletop | undefined
 
     get position(): Readonly<Position> | undefined {
         return this.tabletop ? this.#position : undefined
     }
 
-    placeOnTabletaop(tableTop: Readonly<Tabletop>, initialPosition?: Readonly<Position>): void {
+    get facing(): Readonly<Facing> | undefined {
+        return this.tabletop ? this.#facing : undefined
+    }
+
+    placeOnTabletaop(
+        tableTop: Readonly<Tabletop>,
+        options?: {
+            initialPosition?: Readonly<Position>
+            initialFacing?: Readonly<Facing>
+        },
+    ): void {
         this.tabletop = tableTop
-        if (initialPosition) this.#position = initialPosition
+        if (options?.initialPosition) this.#position = options.initialPosition
+        if (options?.initialFacing) this.#facing = options.initialFacing
     }
 
     move(direction: Direction): void {
@@ -25,15 +38,19 @@ export class Robot {
         switch (direction) {
             case 'UP':
                 if (this.#position.y < this.tabletop.width - 1) ++this.#position.y
+                this.#facing = 'NORTH'
                 break
             case 'DOWN':
                 if (this.#position.y > 0) --this.#position.y
+                this.#facing = 'SOUTH'
                 break
             case 'LEFT':
                 if (this.#position.x > 0) --this.#position.x
+                this.#facing = 'WEST'
                 break
             case 'RIGHT':
                 if (this.#position.x < this.tabletop.height - 1) ++this.#position.x
+                this.#facing = 'EAST'
                 break
         }
     }
